e64c39edea9f25311310280c70afa0f08f092aac
Isaac Blender <isblender@ucla.edu>
2024-04-02 18:51:27 -0700

    Omw to getting conversations to work

--- app/api/conversation_routes.py
+++ app/api/conversation_routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, request, jsonify
+from flask import Blueprint, request, jsonify, request, session
 from .. import db
 from ..models.conversation import Conversation
 
@@ -6,8 +6,15 @@ conversation_blueprint = Blueprint('conversation', __name__)
 
 @conversation_blueprint.route('/', methods=['GET'])
 def get_conversations():
-    conversations = Conversation.query.all()
-    return jsonify([conversation.serialize() for conversation in conversations])
+    data = request.get_json()
+    print(f"data: {data}")
+    initiator_id = data.get('initiator_id')
+    recipient_id = data.get('recipient_id')
+    messages = Conversation.query.filter(
+        ((Conversation.initiator_id == initiator_id) & (Conversation.recipient_id == recipient_id)) |
+        ((Conversation.sender_id == recipient_id) & (Conversation.recipient_id == initiator_id))
+    ).all()
+    
 
 @conversation_blueprint.route('/', methods=['POST'])
 def start_conversation():
--- app/models/apartment.py
+++ app/models/apartment.py
@@ -4,17 +4,15 @@ class Apartment(db.Model):
     __tablename__ = 'apartments'
     apartment_id = db.Column(db.Integer, primary_key=True)
     address = db.Column(db.String(255), nullable=False)
-    city = db.Column(db.String(100), nullable=False)
-    state = db.Column(db.String(100), nullable=False)
-    zip_code = db.Column(db.String(20), nullable=False)
+    school = db.Column(db.Text, nullable=False)
     description = db.Column(db.Text, nullable=False)
     available_from = db.Column(db.Date, nullable=False)
     available_until = db.Column(db.Date, nullable=False)
     created_at = db.Column(db.DateTime, nullable=False, default=db.func.now())
-    number_of_rooms = db.Column(db.Integer, nullable=False)
     owner_id = db.Column(db.Integer, db.ForeignKey('users.user_id'), nullable=False)
     price_per_month = db.Column(db.Float, nullable=False)
-    
+    looking_for_gender = db.Column(db.String(10), nullable=False)
+    looking_for_number = db.Column(db.Integer, nullable=False)
     def serialize(self):
         return {
             'apartment_id': self.apartment_id,
@@ -23,9 +21,9 @@ class Apartment(db.Model):
             'state': self.state,
             'zip_code': self.zip_code,
             'description': self.description,
-            'available_from': self.available_from.isoformat(),
-            'available_until': self.available_until.isoformat(),
-            'created_at': self.created_at.isoformat(),
+            'available_from': self.available_from,
+            'available_until': self.available_until,
+            'created_at': self.created_at,
             'number_of_rooms': self.number_of_rooms,
             'owner_id': self.owner_id,
             'price_per_month': self.price_per_month
